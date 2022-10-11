498565f10f3f637e4f7d07c4ac0775917b231546
albertholskog <albert_holskog@live.no>
2022-10-11 10:02:25 +0200

    reuse-apicall is done

--- js/components/apiMethod.mjs
+++ js/components/apiMethod.mjs
@@ -1,5 +1,6 @@
-import { postData } from "../register.mjs";
-
+import { postData } from "./register.mjs";
+import { loginData } from "./login.mjs";
+import { fetchOptions } from "./get.mjs";
 /**
  *
  *  Global API call for method post.
@@ -20,6 +21,41 @@ export async function postApiCall(url, option = {}) {
     const json = await response.json();
     console.log(json);
     return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+// --------------------------------------------
+
+/**
+ * global login API call for login av bruker
+ * @param {string} url
+ * @param {any} option
+ *
+ * ´´´js
+ * loginUser(loginUrl, loginData);
+ * ´´´
+ */
+export async function loginUser(url, option = {}) {
+  try {
+    option;
+    const response = await fetch(url, loginData);
+    const json = await response.json();
+    const accessToken = json.accessToken;
+    localStorage.setItem("accessToken", accessToken);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// ---------------------------------------------------------
+
+export async function ApicallWithToken(url, option = {}) {
+  try {
+    option;
+    const response = await fetch(url, fetchOptions);
+    const json = await response.json();
+    console.log(json);
   } catch (error) {
     console.log(error);
   }
--- js/scripts.mjs
+++ js/scripts.mjs
@@ -1,6 +1,17 @@
-import { postApiCall } from "./components/apiMethod.mjs";
-import { postData } from "./register.mjs";
+import {
+  postApiCall,
+  loginUser,
+  ApicallWithToken,
+} from "./components/apiMethod.mjs";
+import { postData } from "./components/register.mjs";
+import { loginData } from "./components/login.mjs";
+import { fetchOptions } from "./components/get.mjs";
+
 const API_BASE_URL = "https://nf-api.onrender.com";
 const registerUserUrl = `${API_BASE_URL}/api/v1/social/auth/register`;
+const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
+const postUrl = `${API_BASE_URL}/api/v1/social/posts`;
 
 // postApiCall(registerUserUrl, postData);
+loginUser(loginUrl, loginData);
+ApicallWithToken(postUrl, fetchOptions);
