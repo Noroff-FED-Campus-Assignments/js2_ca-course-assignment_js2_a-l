eded5ca988d8911fb0779480b02cb89cd7062794
Lars Sandell <larssandell@outlook.com>
2022-10-17 11:14:48 +0200

    cleanup

--- js/components/apicall.mjs
+++ js/components/apicall.mjs
@@ -1,17 +1,12 @@
 export async function registerApiCall(url, option = {}) {
     try {
         const response = await fetch(url, option);
-        // console.log(response);
         const json = await response.json();
-        // console.log("se her");
-        // console.log(json);
-        // console.log("se her");
         return json;
     } catch (error) {
         console.log(error);
     }
 }
-// --------------------------------------------
 
 export async function loginUser(url, option = {}) {
     try {
@@ -27,18 +22,12 @@ export async function loginUser(url, option = {}) {
     }
 }
 
-// ---------------------------------------------------------
-
 export async function ApicallWithToken(url, option = {}) {
     try {
         const response = await fetch(url, option);
-        // console.log(response);
         const data = await response.json();
-        // console.log(data);
         return data;
     } catch (error) {
         console.log(error);
     }
 }
-
-// ----------------------------------------------------
